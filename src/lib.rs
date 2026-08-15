27a35b8fcd56942262b0a8b215a619b58062afd6
Lorenzo <medurilorenzo@proton.me>
2026-08-15 17:45:00 +0200

    is_combination_available helper function + tests module with TestEnvironment struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,24 @@
 use common_game::{
-    components::{
-        planet,
-        resource::{
-            self,
-            GenericResource::{self, BasicResources},
-        },
+    components::resource::{
+        ComplexResourceRequest, ComplexResourceType,
+        GenericResource::{self, BasicResources},
     },
     protocols::{
         orchestrator_explorer::{ExplorerToOrchestrator::*, OrchestratorToExplorer::*, *},
-        planet_explorer::{ExplorerToPlanet::*, PlanetToExplorer::*, *},
+        planet_explorer::{PlanetToExplorer::*, *},
     },
     utils::ID,
 };
 use crossbeam_channel::{Receiver, Sender};
 struct Bag {
     resources: Vec<GenericResource>,
+}
+impl Bag {
+    fn new() -> Self {
+        Bag {
+            resources: Vec::new(),
+        }
+    }
 }
 
 struct Explorer {
@@ -29,7 +33,22 @@ struct Explorer {
 }
 
 impl Explorer {
-    fn run(&mut self) {
+    pub fn is_combination_available(&self, resource: ComplexResourceType) -> bool {
+        if let Ok(()) = self
+            .tx_planet
+            .send(ExplorerToPlanet::SupportedCombinationRequest {
+                explorer_id: self.id,
+            })
+        {
+            if let Ok(msg) = self.rx_planet.recv() {
+                if let SupportedCombinationResponse { combination_list } = msg {
+                    return combination_list.contains(&resource);
+                }
+            }
+        }
+        false
+    }
+    pub fn run(&mut self) {
         self.auto_mode = false;
         loop {
             // Checks for a message from the orchestrator
@@ -140,11 +159,91 @@ impl Explorer {
                             }
                         }
                     }
-                    OrchestratorToExplorer::CombineResourceRequest { to_generate } => todo!(),
+                    OrchestratorToExplorer::CombineResourceRequest { to_generate } => { /*
+                        if let Ok(()) =
+                        self.tx_planet
+                        .send(ExplorerToPlanet::CombineResourceRequest {
+                        explorer_id: self.id,
+                        msg: ComplexResourceRequest,
+                        })
+                        {}*/
+                    }
                     BagContentRequest => todo!(),
                     NeighborsResponse { neighbors } => todo!(),
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::HashSet, thread};
+
+    use super::*;
+
+    struct TestEnvironment {
+        // Channel ends of the orchestrator to/from the explorer
+        tx_orchestrator: Sender<OrchestratorToExplorer>,
+        rx_orchestrator: Receiver<ExplorerToOrchestrator<Bag>>,
+        // Channel ends of the planet to/from the explorer
+        tx_planet: Sender<PlanetToExplorer>,
+        rx_planet: Receiver<ExplorerToPlanet>,
+
+        explorer: Explorer,
+    }
+
+    impl Default for TestEnvironment {
+        fn default() -> Self {
+            let (tx_explorer_orchestrator, rx_explorer_orchestrator) =
+                crossbeam_channel::unbounded();
+            let (tx_orchestrator_explorer, rx_orchestrator_explorer) =
+                crossbeam_channel::unbounded();
+            let (tx_explorer_planet, rx_explorer_planet) = crossbeam_channel::unbounded();
+            let (tx_planet_explorer, rx_planet_explorer) = crossbeam_channel::unbounded();
+
+            let explorer = Explorer {
+                id: 0,
+                bag: Bag::new(),
+                planet_id: 0,
+                auto_mode: true,
+                rx_planet: rx_planet_explorer,
+                tx_planet: tx_explorer_planet,
+                rx_orchestrator: rx_orchestrator_explorer,
+                tx_orchestrator: tx_explorer_orchestrator,
+            };
+
+            Self {
+                tx_orchestrator: tx_orchestrator_explorer,
+                rx_orchestrator: rx_explorer_orchestrator,
+                tx_planet: tx_planet_explorer,
+                rx_planet: rx_explorer_planet,
+                explorer,
+            }
+        }
+    }
+    #[test]
+    fn test_is_combination_available() {
+        let environment = TestEnvironment::default();
+        let resource_type = ComplexResourceType::Diamond;
+        let mut combination_list = HashSet::new();
+        combination_list.insert(ComplexResourceType::Diamond);
+        thread::scope(|t| {
+            t.spawn(|| {
+                if let Ok(msg) = environment.rx_planet.recv() {
+                    if let ExplorerToPlanet::SupportedCombinationRequest { explorer_id } = msg {
+                        if let Ok(()) = environment
+                            .tx_planet
+                            .send(SupportedCombinationResponse { combination_list })
+                        {
+                        }
+                    }
+                }
+            });
+            assert_eq!(
+                environment.explorer.is_combination_available(resource_type),
+                true
+            );
+        });
     }
 }
